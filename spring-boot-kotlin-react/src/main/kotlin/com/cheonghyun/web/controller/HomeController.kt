18c21a03e3aead040906bfab9aa8d7df0c352ad9
Cheonghyun Lee <lee@cheonghyun.com>
2024-08-16 03:51:47 +0900

    Initial commit

---
+++ spring-boot-kotlin-react/src/main/kotlin/com/cheonghyun/web/controller/HomeController.kt
@@ -0,0 +1,14 @@
+package com.cheonghyun.web.controller
+
+import ch.qos.logback.core.model.Model
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HomeController {
+
+    @GetMapping("/")
+    fun home(model: Model): String {
+        return "home"
+    }
+}
